ae45bc19caf5e6d198819700ec666dc10a8c0fb4
GeralPinzon <geraldinpinzonb@gmail.com>
2021-08-29 23:44:46 -0500

    Geral Pinzon: Correción detección Mano Izquierda

--- gesture_detector.py
+++ gesture_detector.py
@@ -10,17 +10,30 @@ with mp_hands.Hands(
    max_num_hands=1,
    min_detection_confidence=0.5 ) as hands:
 
-   image = cv2.imread("tres.jpeg")
+   image = cv2.imread("I_Uno_A.jpeg")
+   #image = cv2.imread("I_Dos_A.jpeg")
+   #image = cv2.imread("I_Tres_A.jpeg")
+   #image = cv2.imread("I_Cuatro_A.jpeg")
+   #image = cv2.imread("D_Cinco_A_Borrosa.jpeg")
+   #image = cv2.imread("I_Cinco_A.jpeg")
+   #image = cv2.imread("I_Rock_A.jpeg")
+   #image = cv2.imread("I_Spider_A.jpeg")
+
    height, width, _ = image.shape
-   image = cv2.flip(image, 1)
+   image = cv2.flip(image, 1) #Comentar si se usa la camara frontal
 
    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
    
-   results = hands.process(image_rgb) 
-   print(results.multi_handedness)
+   results = hands.process(image_rgb)
+   label = results.multi_handedness[0].classification[0].label
    
-   if results.multi_hand_landmarks:
+   print(results.multi_handedness)
+      
+   if results.multi_hand_landmarks is not None:
+      
+      label = results.multi_handedness[0].classification[0].label  # label gives if hand is left or right
       for handLandmarks in results.multi_hand_landmarks:
+            
             mp_drawing.draw_landmarks(image, handLandmarks, mp_hands.HAND_CONNECTIONS)
             thumbIsOpen = False
             indexIsOpen = False
@@ -29,14 +42,18 @@ with mp_hands.Hands(
             pinkyIsOpen = False
          
             pseudoFixKeyPoint = handLandmarks.landmark[2].x
-            if handLandmarks.landmark[3].x < pseudoFixKeyPoint and handLandmarks.landmark[4].x < pseudoFixKeyPoint:
-               thumbIsOpen = True
+
+            if label == 'Right':
+               if handLandmarks.landmark[3].x < pseudoFixKeyPoint and handLandmarks.landmark[4].x < pseudoFixKeyPoint:
+                  thumbIsOpen = True
+            elif label == 'Left':
+               if handLandmarks.landmark[3].x > pseudoFixKeyPoint and handLandmarks.landmark[4].x > pseudoFixKeyPoint:
+                  thumbIsOpen = True
 
             pseudoFixKeyPoint = handLandmarks.landmark[6].y
             if handLandmarks.landmark[7].y < pseudoFixKeyPoint and handLandmarks.landmark[8].y < pseudoFixKeyPoint:
                indexIsOpen = True
          
-
             pseudoFixKeyPoint = handLandmarks.landmark[10].y
             if handLandmarks.landmark[11].y < pseudoFixKeyPoint and handLandmarks.landmark[12].y < pseudoFixKeyPoint:
                middelIsOpen = True
@@ -50,19 +67,19 @@ with mp_hands.Hands(
                pinkyIsOpen = True
 
             if thumbIsOpen and indexIsOpen and middelIsOpen and ringIsOpen and pinkyIsOpen:
-               print("FIVE!")
+               print("CINCO!")
 
             elif not thumbIsOpen and indexIsOpen and middelIsOpen and ringIsOpen and pinkyIsOpen:
-               print("FOUR!")
+               print("CUATRO!")
 
             elif not thumbIsOpen and indexIsOpen and middelIsOpen and ringIsOpen and not pinkyIsOpen:
-               print("THREE!")
+               print("TRES!")
 
             elif not thumbIsOpen and indexIsOpen and middelIsOpen and not ringIsOpen and not pinkyIsOpen:
-               print("TWO!")
+               print("DOS!")
 
             elif not thumbIsOpen and indexIsOpen and not middelIsOpen and not ringIsOpen and not pinkyIsOpen:
-               print("ONE!")
+               print("UNO!")
 
             elif not thumbIsOpen and indexIsOpen and not middelIsOpen and not ringIsOpen and pinkyIsOpen:
                print("ROCK!")
@@ -71,11 +88,16 @@ with mp_hands.Hands(
                print("SPIDERMAN!")
 
             elif not thumbIsOpen and not indexIsOpen and not middelIsOpen and not ringIsOpen and not pinkyIsOpen:
-               print("FIST!")
+               print("PUÑO!")
+            elif  thumbIsOpen and indexIsOpen and not middelIsOpen and not ringIsOpen and not pinkyIsOpen:
+               print("LUNES!")
    
+
             print("FingerState: thumbIsOpen? " + str(thumbIsOpen) + " - indexIsOpen? " + str(indexIsOpen) + " - middelIsOpen? " +
             str(middelIsOpen) + " - ringIsOpen? " + str(ringIsOpen) + " - pinkyIsOpen? " + str(pinkyIsOpen))
+            
 image = cv2.resize(image, (600, 600))
+image = cv2.flip(image, 1) 
 cv2.imshow("Image",image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
